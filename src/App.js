cbba8fe9b3cf4bf9eb04cdc917df7665f156abc7
aum <techtv49@gmail.com>
2023-02-12 00:54:53 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -2,24 +2,21 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route}  from 'react-router-dom';
 import Doctorpanel from './components/doctor_panel';
-import Sidebar from "./components/Sidebar";
 import PatientPanel from "./components/PatientPanel";
 import Monitoringpanel from "./components/Monitoringpanel";
 import Login from './components/Loginpage';
 import data from "./components/json/datapatient.json"
-import Grafena from './components/grafena';
 import Navbar from './components/navbar';
 function App(){
   return (
    <React.Fragment>
-   <Navbar/>
-      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+    <Navbar/>
       <Router>
       <Routes>
         <Route exact0 path='/' exact element={<Login/>} />
         <Route exact1 path='/doctorpanel' exact element={<Doctorpanel />} />
         <Route exact2 path='/patientpanel' exact element={<PatientPanel roomNumber={data.roomNumber}  patientInfo={data.patientInfo} />} />
-        <Route exact3 path='/monitoringpanel' exact element={<Grafena />} />
+        <Route exact3 path='/monitoringpanel' exact element={<Monitoringpanel />} />
 
   {/* <Switch>
      <Navbar isAuth={loggedIn} />
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import './navbar.css';
+import Sidebar from './Sidebar';
 function Navbar() {
   return (
+    
     <nav className="navbar">
+    <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
       <div className="navbar-left">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a href="#" className="nav-link">Home</a>
+            <a href="/" className="nav-link">Home</a>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link">Doctor Panel</a>
+            <a href="/doctorpanel" className="nav-link">Doctor Panel</a>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link">Patient Panel</a>
+            <a href="/patientpanel" className="nav-link">Patient Panel</a>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link">Monitoring Panel</a>
+            <a href="/monitoringpanel" className="nav-link">Monitoring Panel</a>
           </li>
         </ul>
       </div>
